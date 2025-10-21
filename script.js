28823bb38494062620e3833409a3f7ab1a52f108
JoseLConte <jose.conte@escola.pr.gov.br>
2025-10-21 10:19:23 -0300

    21/10

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const botaoMostraPalavras = document.querySelector("botao-palavrachave");
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){
-    const texto = document.querrySelector("#entrada-de-texto").value;
-    const campoResultado = document.querrySelector("#resultado-palavrachave");
-    const palavars = texto.split(" ");
+function mostraPalavrasChave() {
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavras = texto.split(" ");
 
-    campoResultado.textContent = palavaras.join(", ")
+    campoResultado.textContent = palavras.join(", ");
 }
